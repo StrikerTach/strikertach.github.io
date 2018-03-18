e0f39be8342c78ecef2910315749ac99de8c1afa
StrikerTach <angry_monster@mail.ru>
2018-03-18 16:18:13 +0700

    init

---
+++ ДЗ. Горбачев Илья. Первый урок/Untitled-1.c
@@ -0,0 +1,14 @@
+#include <studio.h>
+int main()
+{
+int count;
+
+    scanf("%d", &count);
+        if (count == 10){
+    printf("Hello, world");
+    }
+        else {
+    printf("Goodbye, world!");
+    }
+        return 0;
+}
